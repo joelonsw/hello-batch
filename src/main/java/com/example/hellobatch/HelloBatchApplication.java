716e00e2050cf4fd8a8c6d12eff499520e5b5fb5
joelonsw <joel610naver@gmail.com>
2023-09-13 10:18:50 +0900

    feat: 배치 프로젝트 초기 셋업

---
+++ src/main/java/com/example/hellobatch/HelloBatchApplication.java
@@ -0,0 +1,15 @@
+package com.example.hellobatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class HelloBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBatchApplication.class, args);
+	}
+
+}
